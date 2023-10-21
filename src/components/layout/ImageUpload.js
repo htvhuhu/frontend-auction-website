ce0ad2ae92c401692ac80fd6bc037fae9f78cd67
BNB <butbuiapp@gmail.com>
2023-10-21 12:21:39 -0500

    update layout of create product

--- src/components/layout/ImageUpload.js
+++ src/components/layout/ImageUpload.js
@@ -1,33 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import productService from '../../services/ProductService';
 
-const ImageUpload = ({setImgs}) => {
+const ImageUpload = ({ setImgs }) => {
 
-    const onFileChange = async (e) => {
-        const files = e.target.files;
-        const formData = new FormData();
-        
-        for (let file of files) { // Loop through and append each file
-            formData.append('files', file);
-        }
-    
-        try {
-            const response = await productService.uploadProductImages(formData);
-            setImgs(response.data);
-        } catch (error) {
-            console.error("There was an error uploading the files!", error);
-            alert("There was an error uploading the files!");
-        }
-    };
+  const onFileChange = async (e) => {
+    const files = e.target.files;
+    const formData = new FormData();
 
-    
+    for (let file of files) { // Loop through and append each file
+      formData.append('files', file);
+    }
 
-    return (
-        <div>
-            <input type="file" onChange={onFileChange} multiple />
-            {/* <button type='button' onClick={onUpload} disabled={files?.length === 0} >Upload</button> */}
-        </div>
-    );
+    try {
+      const response = await productService.uploadProductImages(formData);
+      setImgs(response.data);
+    } catch (error) {
+      console.error("There was an error uploading the files!", error);
+      alert("There was an error uploading the files!");
+    }
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={onFileChange} multiple />
+    </div>
+  );
 };
 
 export default ImageUpload;
--- src/pages/seller/CreateProduct.js
+++ src/pages/seller/CreateProduct.js
@@ -1,254 +1,266 @@
 import React, { useContext, useEffect, useState } from 'react';
 import productService from '../../services/ProductService';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import '../../css/pages/Seller.css';
 import ImageUpload from '../../components/layout/ImageUpload';
-import {useNavigate, useParams} from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { PRODUCT_STATUS } from '../../util/constant';
 import Select from 'react-select';
 import { toast } from 'react-toastify';
 import { AuthContext } from '../../services/AuthProvider';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 
 const CreateProduct = () => {
-    const today = new Date().toISOString().split('T')[0];
-    const categories = [
-        'Electronics', 'Clothing', 'Books', 'Home Appliances', 
-        'Automobiles', 'Beauty & Personal Care', 'Computers', 'Furniture', 
-        'Gardening', 'Groceries', 'Health & Fitness', 'Jewelry', 
-        'Kitchen Appliances', 'Musical Instruments', 'Pets', 'Sports Equipment',
-        'Toys & Games', 'Phones & Accessories', 'Outdoor', 'Travel & Luggage'
-    ].map(category => ({ label: category, value: category }));
+  const today = new Date().toISOString().split('T')[0];
+  const categories = [
+    'Electronics', 'Clothing', 'Books', 'Home Appliances',
+    'Automobiles', 'Beauty & Personal Care', 'Computers', 'Furniture',
+    'Gardening', 'Groceries', 'Health & Fitness', 'Jewelry',
+    'Kitchen Appliances', 'Musical Instruments', 'Pets', 'Sports Equipment',
+    'Toys & Games', 'Phones & Accessories', 'Outdoor', 'Travel & Luggage'
+  ].map(category => ({ label: category, value: category }));
 
-    const { email } = useContext(AuthContext);
-    const [product, setProduct] = useState({ 
-        name: '', 
-        description: '', 
-        categories: [],
-        deposit: '', 
-        bidStartPrice: '', 
-        bidDueDate: today,
-        paymentDueDate: today,
-        status: '',
-        images:[],
-        conditionOfSale:"",
-        shippingInformation:"",
-        owner: email,
-        created: new Date()
-    });
-    const navigate = useNavigate();
-    const setImages = (images)=>{
-        const newImages = images.map((img,index)=>({id: product?.images[index]?.id, name:img.name}));
-        setProduct({ ...product, images: newImages});
+  const { email } = useContext(AuthContext);
+  const [product, setProduct] = useState({
+    name: '',
+    description: '',
+    categories: [],
+    deposit: '',
+    bidStartPrice: '',
+    bidDueDate: today,
+    paymentDueDate: today,
+    status: '',
+    images: [],
+    conditionOfSale: "",
+    shippingInformation: "",
+    owner: email,
+    created: new Date()
+  });
+  const navigate = useNavigate();
+  const setImages = (images) => {
+    const newImages = images.map((img, index) => ({ id: product?.images[index]?.id, name: img.name }));
+    setProduct({ ...product, images: newImages });
+  }
+
+  const params = useParams();
+  useEffect(() => {
+    if (params?.id) {
+      (async () => {
+        const response = await productService.getProductsById(params.id);
+        setProduct(response.data);
+      })();
     }
+  }, [params.id])
 
-    const params = useParams();
-    useEffect(()=>{
-        if(params?.id){
-            (async ()=>{
-                const response = await productService.getProductsById(params.id);
-                setProduct(response.data);
-            })();
+  const handleChange = (e) => {
+    setProduct({ ...product, [e.target.name]: e.target.value });
+  };
+
+  const handleCategoryChange = (selectedOptions) => {
+    setProduct({
+      ...product, categories: selectedOptions ? selectedOptions.map(o => o.value) : []
+    });
+  };
+
+
+  const handleSave = async (release) => {
+    try {
+      if (release === "release") {
+        const requiredFields = [
+          'name',
+          'description',
+          'deposit',
+          'bidStartPrice',
+          'bidDueDate',
+          'paymentDueDate',
+          'conditionOfSale',
+          'shippingInformation'
+        ];
+
+        const isAnyFieldEmpty = requiredFields.some(field => !product[field] || product[field] === '');
+        const areCategoriesEmpty = !product.categories || product.categories.length === 0;
+
+        if (isAnyFieldEmpty || areCategoriesEmpty) {
+          toast.error("Please input all information to release!");
+          return;
         }
-    },[params.id])
+      }
+      const newProduct = { ...product, status: release, owner: email };
+      console.log(newProduct);
+      if (params?.id) {
+        await productService.updateProduct(params.id, newProduct);
+      } else {
+        await productService.addProduct(newProduct);
+      }
+      toast.success('Product saved successfully!');
+    } catch (error) {
+      console.error('There was an error!', error);
+      toast.error('There was an error saving the product.');
 
-    const handleChange = (e) => {
-        setProduct({ ...product, [e.target.name]: e.target.value });
-    };
+    }
+    navigate("/seller/products");
+  };
 
-    const handleCategoryChange = (selectedOptions) => {
-            setProduct({...product, categories: selectedOptions ? selectedOptions.map(o=>o.value) : []
-        });
-    };
-    
+  return (
+    <div className="container mt-3 seller-product-add-container">
+      <div className='box'>
+        <h2>{params?.id ? "Edit Product" : "Add Product"}</h2>
+        <form className='seller-product-add-form' onSubmit={(e) => e.preventDefault()}>
 
-    const handleSave = async (release) => {
-        try {
-            if (release === "release") {
-                const requiredFields = [
-                    'name', 
-                    'description', 
-                    'deposit', 
-                    'bidStartPrice', 
-                    'bidDueDate', 
-                    'paymentDueDate', 
-                    'conditionOfSale', 
-                    'shippingInformation'
-                ];
-            
-                const isAnyFieldEmpty = requiredFields.some(field => !product[field] || product[field] === '');
-                const areCategoriesEmpty = !product.categories || product.categories.length === 0;
-            
-                if (isAnyFieldEmpty || areCategoriesEmpty) {
-                    toast.error("Please input all information to release!");
-                    return;
-                }
-            }
-            const newProduct = { ...product, status: release,owner:email };
-            console.log(newProduct);
-            if(params?.id){
-                await productService.updateProduct(params.id,newProduct);
-            }else{
-                await productService.addProduct(newProduct);
-            }
-            toast.success('Product saved successfully!');
-        } catch (error) {
-            console.error('There was an error!', error);
-            toast.error('There was an error saving the product.');
+          <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={product.name}
+              onChange={handleChange}
+              className="form-control"
+              required
+            />
+          </div>
 
-        }
-        navigate("/seller/products");
-    };
+          <div className="form-group">
+            <label htmlFor="description">Description</label>
+            <textarea
+              id="description"
+              name="description"
+              value={product.description}
+              onChange={handleChange}
+              className="form-control"
+              required
+            />
+          </div>
 
-    return (
-        <div className="container mt-3 seller-product-add-container">
-            <h2>{params?.id ? "Edit Product" : "Add Product"}</h2>
-            <form className='seller-product-add-form' onSubmit={(e) => e.preventDefault()}>
-                 
-            <div className="form-group">
-                <label htmlFor="name">Name</label>
-                <input 
-                    type="text" 
-                    id="name"
-                    name="name" 
-                    value={product.name} 
-                    onChange={handleChange} 
-                    className="form-control" 
-                    required 
-                />
-            </div>
+          <div className="form-group">
+            <label htmlFor="categories">Categories</label>
+            <Select
+              id="categories"
+              name="categories"
+              options={categories}
+              value={categories.filter(option => product.categories.includes(option.value))}
+              onChange={handleCategoryChange}
+              className="form-control"
+              isMulti
+              isSearchable
+            />
+          </div>
 
-            <div className="form-group">
-                <label htmlFor="description">Description</label>
-                <textarea 
-                    id="description"
-                    name="description" 
-                    value={product.description} 
-                    onChange={handleChange} 
-                    className="form-control"
-                    required 
-                />
-            </div>
+          <div className="form-group">
+            <label htmlFor="shippingInformation">Shipping Information</label>
+            <textarea
+              id="shippingInformation"
+              name="shippingInformation"
+              value={product.shippingInformation}
+              onChange={handleChange}
+              className="form-control"
+              required
+            />
+          </div>
 
-            <div className="form-group">
-                <label htmlFor="categories">Categories</label>
-                <Select 
-                    id="categories"
-                    name="categories"
-                    options={categories} 
-                    value={categories.filter(option => product.categories.includes(option.value))}
-                    onChange={handleCategoryChange} 
-                    className="form-control"
-                    isMulti
-                    isSearchable
-                />
-            </div>
+          <div className="form-group">
+            <label htmlFor="conditionOfSale">Condition Of Sale</label>
+            <textarea
+              id="conditionOfSale"
+              name="conditionOfSale"
+              value={product.conditionOfSale}
+              onChange={handleChange}
+              className="form-control"
+              required
+            />
+          </div>
 
-            <div className="form-group">
-                <label htmlFor="shippingInformation">Shipping Information</label>
-                <textarea 
-                    id="shippingInformation"
-                    name="shippingInformation" 
-                    value={product.shippingInformation} 
-                    onChange={handleChange} 
-                    className="form-control"
-                    required 
-                />
-            </div>
+          <div className="form-group">
+            <label htmlFor="bidStartPrice">Starting Price</label>
+            <input
+              type="number"
+              id="bidStartPrice"
+              name="bidStartPrice"
+              value={product.bidStartPrice}
+              onChange={handleChange}
+              className="form-control"
+              required
+            />
+          </div>
 
-            <div className="form-group">
-                <label htmlFor="conditionOfSale">Condition Of Sale</label>
-                <textarea 
-                    id="conditionOfSale"
-                    name="conditionOfSale" 
-                    value={product.conditionOfSale} 
-                    onChange={handleChange} 
-                    className="form-control"
-                    required 
-                />
-            </div>
+          <div className="form-group">
+            <label htmlFor="deposit">Deposit</label>
+            <input
+              type="number"
+              id="deposit"
+              name="deposit"
+              value={product.deposit}
+              onChange={handleChange}
+              className="form-control"
+              required
+            />
+          </div>
 
-            <div className="form-group">
-                <label htmlFor="bidStartPrice">Starting Price</label>
-                <input 
-                    type="number" 
-                    id="bidStartPrice"
-                    name="bidStartPrice" 
-                    value={product.bidStartPrice} 
-                    onChange={handleChange} 
-                    className="form-control" 
-                    required 
-                />
-            </div>
+          <div className="form-group">
+            <label htmlFor="bidDueDate">Bid Due Date</label>
+            <input
+              type="date"
+              id="bidDueDate"
+              name="bidDueDate"
+              value={product.bidDueDate}
+              onChange={handleChange}
+              className="form-control"
+              required
+            />
+          </div>
 
-            <div className="form-group">
-                <label htmlFor="deposit">Deposit</label>
-                <input 
-                    type="number" 
-                    id="deposit"
-                    name="deposit" 
-                    value={product.deposit} 
-                    onChange={handleChange} 
-                    className="form-control" 
-                    required 
-                />
-            </div>
+          <div className="form-group">
+            <label htmlFor="paymentDueDate">Payment Due Date</label>
+            <input
+              type="date"
+              id="paymentDueDate"
+              name="paymentDueDate"
+              value={product.paymentDueDate}
+              onChange={handleChange}
+              className="form-control"
+              required
+            />
+          </div>
 
-            <div className="form-group">
-                <label htmlFor="bidDueDate">Bid Due Date</label>
-                <input 
-                    type="date" 
-                    id="bidDueDate"
-                    name="bidDueDate" 
-                    value={product.bidDueDate} 
-                    onChange={handleChange} 
-                    className="form-control"
-                    required 
-                />
-            </div>
+          <ImageUpload setImgs={setImages} />
 
-            <div className="form-group">
-                <label htmlFor="paymentDueDate">Payment Due Date</label>
-                <input 
-                    type="date" 
-                    id="paymentDueDate"
-                    name="paymentDueDate" 
-                    value={product.paymentDueDate} 
-                    onChange={handleChange} 
-                    className="form-control" 
-                    required 
-                />
-            </div>
-            
-            <ImageUpload setImgs={setImages} />
-                
-                <div>
-                    {product.images?.map((image) => (
-                        <img key={image.id} src={productService.getProductImage(image.name)} alt={image.name} height={100} width={100} />
-                    ))}
-                </div>
+          <div>
+            {product.images?.map((image) => (
+              <img key={image.id} src={productService.getProductImage(image.name)} alt={image.name} height={100} width={100} className='me-2'/>
+            ))}
+          </div>
 
-                <div>
-                    
-                <button 
-                    type="button" 
+          <div>
+            <Container>
+              <Row>
+                <Col>
+                  <button
+                    type="button"
                     onClick={() => handleSave(`${PRODUCT_STATUS.DRAFT}`)}
-                    style={{width:"50%"}}
-                    className="btn btn-primary mr-5">
+                    style={{ width: "100%" }}
+                    className="btn btn-primary me-5">
                     Save draft
-                </button>
-
-                <button 
-                    type="button" 
-                    onClick={() => handleSave(`${PRODUCT_STATUS.RELEASE}`)} 
-                    style={{width:"50%"}}
+                  </button>
+                </Col>
+                <Col>
+                  <button
+                    type="button"
+                    onClick={() => handleSave(`${PRODUCT_STATUS.RELEASE}`)}
+                    style={{ width: "100%" }}
                     className="btn btn-success">
                     Release
-                </button>
-                
-                </div>
-            </form>
-        </div>
-    );
+                  </button>
+                </Col>
+              </Row>
+            </Container>
+
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default CreateProduct;
