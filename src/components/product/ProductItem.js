e88abbed34f038f9e48f5ec6ce7b3122c052740c
BNB <butbuiapp@gmail.com>
2023-10-21 20:03:26 -0500

    add closing soon to product list

--- src/components/product/ProductItem.js
+++ src/components/product/ProductItem.js
@@ -1,13 +1,11 @@
 import Card from 'react-bootstrap/Card';
 import { useNavigate } from 'react-router-dom';
 import productService from '../../services/ProductService';
-import { formatDate } from '../../util/dateTimeUtil';
+import { formatDate, isClosingSoon } from '../../util/dateTimeUtil';
 
 function ProductItem({ prod }) {
   const navigate = useNavigate();
 
-  console.log('product===', prod);
-  
   function goToProductDetail() {
     navigate("/product/detail", {state: prod.id});
   }
@@ -26,6 +24,15 @@ function ProductItem({ prod }) {
               <div className='py-2'>
                 <b>#ID:</b>
                 <label className='ms-2'>{prod.id}</label>
+                {isClosingSoon(prod.bidDueDate) &&
+                <span className='text-danger ms-5'>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-clock" viewBox="0 0 16 16">
+                    <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71V3.5z" />
+                    <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0z" />
+                  </svg>
+                  <label className='ms-2'>Closing Soon!</label>
+                </span>
+                }
               </div>
               <div>
                 <b>Bid Due Date:</b>
--- src/util/dateTimeUtil.js
+++ src/util/dateTimeUtil.js
@@ -2,3 +2,10 @@
 export const formatDate = (date) => {
   return new Date(date).toLocaleString();
 }
+
+export const isClosingSoon = (date) => {
+  const currentDate = new Date().toLocaleDateString();
+  let dueDate = new Date(date).toLocaleDateString();
+  if (currentDate === dueDate) return true;
+  return false;
+}
